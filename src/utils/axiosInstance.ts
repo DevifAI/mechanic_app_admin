07f4f802bee7f2f3e16a4576b60d5627f8b18a8d
sayanpal469 <sayanpal469@gmail.com>
2025-06-20 15:18:59 +0530

    hello

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,20 +1,18 @@
 // utils/axiosInstance.ts
 import axios from "axios";
 
+const isProduction = true; // Change this to false for development
+
 const url = {
-  production:
-    "https://www.devifai.website/api/master/super/admin",
+  production: "https://www.devifai.website/api/master/super/admin",
   development: "http://localhost:5000/api/master/super/admin",
 };
 
 const axiosInstance = axios.create({
-
-  baseURL: url.production, // Change to url.development for local testing
-
+  baseURL: isProduction ? url.production : url.development,
   headers: {
     "Content-Type": "application/json",
   },
-  // You can add interceptors if needed (e.g., for auth tokens)
 });
 
 export default axiosInstance;
